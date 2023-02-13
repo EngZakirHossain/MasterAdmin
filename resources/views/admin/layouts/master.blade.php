9c0fd44ff08ed3e60fe5258c07ce38717d4313fe
Zakir Trimatric <zakirtrimatric@gmail.com>
2023-02-13 14:47:38 +0600

    Small modification done

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>ZN | @yield('page_title')</title>
+    <title>{{ setting('site_title') }} | @yield('page_title')</title>
 
     <meta name="description" content="" />
 
