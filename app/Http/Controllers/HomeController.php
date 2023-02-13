5a33a37ade4676f205293f0cbd85603e7cf4704d
Zakir Trimatric <zakirtrimatric@gmail.com>
2023-02-13 17:57:22 +0600

    Dashboard Modification

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
@@ -26,6 +27,7 @@ class HomeController extends Controller
     {
         //authorize this user to access or not
         Gate::authorize('access-dashboard');
-        return view('admin.pages.dashboard');
+        $user = User::with('role')->latest('id')->get();
+        return view('admin.pages.dashboard',compact('user'));
     }
 }
